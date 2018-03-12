c900a57a61f3efe513ff4a3433738b64062e0196
Mike Lang <mikelang3000@gmail.com>
2018-03-12 01:46:16 -0700

    Make audio_data banks derive from a constant

---
+++ include/banks.asm
@@ -0,0 +1,6 @@
+IF !DEF(_G_BANKS)
+_G_BANKS EQU "true"
+
+AUDIO_BANK_START = 1
+
+ENDC
--- tools/quantize_audio.py
+++ tools/quantize_audio.py
@@ -6,7 +6,7 @@ ceil = max(values)
 
 BANK_SIZE = 16*1024
 
-def main(asm_path, first_bank=1):
+def main(asm_path):
 	xs = []
 	ys = []
 	while True:
@@ -36,10 +36,11 @@ def main(asm_path, first_bank=1):
 	ys = ys + [7] * 32
 
 	with open(asm_path, 'w') as f:
+		f.write('include "banks.asm"\n')
 		for n, ((x1, x2), (y1, y2)) in enumerate(zip(zip(xs[::2], xs[1::2]), zip(ys[::2], ys[1::2]))):
 			n = n * 2
 			if n % BANK_SIZE == 0:
-				f.write('SECTION "Audio data part {}", ROMX, BANK[{}]\n'.format(n, first_bank+n/BANK_SIZE))
+				f.write('SECTION "Audio data part {}", ROMX, BANK[AUDIO_BANK_START + {}]\n'.format(n, n/BANK_SIZE))
 			# pair of volumes first, then pair of samples. in both cases first sample is most signifigant nibble
 			f.write("\tdb ${:x}{:x}\n".format(x1, x2))
 			f.write("\tdb ${:x}{:x}\n".format(y1, y2))
