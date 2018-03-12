edd18594ce6c9d1f84916e1d8567ed27c4bb9a56
don <sleepyparadox@gmail.com>
2018-03-12 22:59:57 +1100

    serialize (fake) palette groups and textures (at right angles?)

--- tools/windows/frameparse/Frames/TextureLoadOrder.cs
+++ tools/windows/frameparse/Frames/TextureLoadOrder.cs
@@ -8,10 +8,24 @@ namespace animparse.Frames
 {
     public class TextureLoadOrder
     {
+        // can't copy over boundaries 1024
         public int SourceIndex;
         public int DestinationBank;
         public int DestinationIndex;
         public int TexturesToCopy;
+
+        const int LoadBoundaries = 102;
+
+        public void ValidateBanks()
+        {
+            var startBank = SourceIndex / 1024;
+            var endBank = (SourceIndex + TexturesToCopy) / 1024;
+
+            if (startBank != endBank)
+            {
+                throw new Exception("Can't copy between banks");
+            }
+        }
 
         public override string ToString()
         {
